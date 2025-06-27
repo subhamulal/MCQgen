f6728bfcce722a65ab66d148e482ee43446b1501
Subham ulal <subhamulal58@gmail.com>
2025-06-27 12:39:16 +0530

    Add

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='subham ulal',
+    author_email='subhamulal58@gmail.com',
     install_requires=["openai","langchain","langchain-community","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
